f3b673d44d4d4909faa6ab11b924f7160a5c7fc7
Edzard <231426226+edzardVOID@users.noreply.github.com>
2025-11-11 04:43:42 +0000

    Create progress.js

---
+++ javacript/progress.js
@@ -0,0 +1,32 @@
+//#String
+;Pembelajaran String
+console.log('udinpekok');
+
+//#Variable
+//#Peraturan VARIABLE Harus pakai camelcase agar konsisten
+const UdinA = 'udindindin'
+    console.log(UdinA);
+    console.log(UdinA);
+    console.log(UdinA);
+
+//#Aturan Variable
+const myName = 'udin'
+const myLastname = 'pekok'
+const myHobbie = 'ngerokok'
+const umur = '200'
+    console.log(myName);
+    console.log(myLastname);
+    console.log(myHobbie);
+    console.log(umur);
+
+//#String contcatenation
+const myName = 'udin'
+const myLastname = 'pekok'
+const myHobbie = 'ngerokok'
+const umur = '200'
+const fullname = myName + ' ' + myLastname;
+    console.log(fullname);
+    console.log(myName);
+    console.log(myLastname);
+    console.log(myHobbie);
+    console.log(umur);
