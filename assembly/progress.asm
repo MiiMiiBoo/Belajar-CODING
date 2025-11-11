063619d48d97566dbd6850b1ce3142c5291d8a7c
Edzard <231426226+edzardVOID@users.noreply.github.com>
2025-11-11 04:53:17 +0000

    Update progress.asm

--- assembly/progress.asm
+++ assembly/progress.asm
@@ -16,4 +16,56 @@ section .text
         mov eax, 1          ; syscall: sys_exit
         xor ebx, ebx        ; exit code 0
         int 0x80            ; call kernel
-        
+
+
+global _start
+
+section .data; memakai bilangan decimal
+	huruf: db "udin", 10
+
+section .text
+	_start:
+		mov ecx, huruf
+		mov edx, 4
+		mov ebx, 1
+		mov eax, 4
+		int 128
+		
+		mov ebx, 0
+		mov eax, 1
+		int 128
+
+global _start
+
+section .data; memakai bilangan biner
+	huruf: db 01000001b, 00001010b
+	
+section .text
+	_start:
+		mov ecx, huruf
+		mov edx, 2
+		mov ebx, 00000001b
+		mov eax, 00000100b
+		int 10000000b
+				
+		mov ebx, 00000000b
+		mov eax, 000000001b
+		int 10000000b
+
+global _start
+
+section .data
+	udin : db "wala", 4
+	
+section .text
+	_start :
+		mov ecx, udin
+		mov edx, 4
+		mov ebx, 1
+		mov eax, 4
+		int 128
+		
+		mov ebx, 0
+		mov eax, 1
+		int 128q
+		
