48f25584a819c88d1eb7256cb232e7dcf06f98d6
edzardVOID <404kidzure@gmail.com>
2025-11-12 19:33:23 +0700

    a

--- c++/progress.cpp
+++ c++/progress.cpp
@@ -5,3 +5,37 @@ int main()
 {
     std::cout << "hellow world";
 }
+
+/*
+ini merupakan komentar multi line 
+jadi tidak perlu manual garis miring (/)
+*/
+#include <iostream>
+
+int main()
+{
+    std::cout << "hellow world";
+}
+
+/*bagaimana jika tidak ingin menggunakan std::cout
+ jadi bisa menggunakan cout saja contoh*/
+
+ #include <iostream>
+using namespace std;
+ int main ()
+ {
+    cout << "hellow world";      /*contoh penghilangan std::*/
+ }
+
+ /*ada 4 tipe data fundamental dalam c++*/
+ #include <iostream>
+ using name space std;
+
+ int main ()
+ {
+    char karaktersaya;
+    int panjang;
+    int lebar;
+    float phi;
+    bool a;
+ }
